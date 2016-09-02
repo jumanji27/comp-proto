c51f8610d7ece0aa06f0f369839d5ecd7568f25d
Nikita Lebedev <nikita.lebedev@lazada.com>
2016-09-02 14:17:28 +0300

    gofmt

--- server/comp_proto.go
+++ server/comp_proto.go
@@ -1,68 +1,65 @@
 package main
 
-
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/go-martini/martini"
-    "github.com/martini-contrib/render"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
 )
 
-
-type Main struct {}
+type Main struct{}
 
 func (self *Main) route(app *martini.ClassicMartini) {
-    const (
-        http_success = 200
-    )
+	const (
+		http_success = 200
+	)
 
-    app.Get(
-        "/",
-        func(render render.Render) {
-            render.HTML(http_success, "index", nil)
-        },
-    )
+	app.Get(
+		"/",
+		func(render render.Render) {
+			render.HTML(http_success, "index", nil)
+		},
+	)
 
-    app.Post(
-        "/api/v1/banner",
-        func(render render.Render) {
-            render.JSON(
-                http_success,
-                // Mocks
-                map[string]interface{}{
-                    "success": map[string]interface{}{
-                        "title": "MasterCard Monday",
-                        "desc": "Get 10% off your purchase storewide when you pay with your MasterCard",
-                        "button_text": "Shop now",
-                        "img": "storage/banner.jpg",
-                        "discount": 10,
-                    },
-                    "errors": nil,
-                },
-            )
-        },
-    )
+	app.Post(
+		"/api/v1/banner",
+		func(render render.Render) {
+			render.JSON(
+				http_success,
+				// Mocks
+				map[string]interface{}{
+					"success": map[string]interface{}{
+						"title":       "MasterCard Monday",
+						"desc":        "Get 10% off your purchase storewide when you pay with your MasterCard",
+						"button_text": "Shop now",
+						"img":         "storage/banner.jpg",
+						"discount":    10,
+					},
+					"errors": nil,
+				},
+			)
+		},
+	)
 }
 
-
 func main() {
-    martini_app := martini.Classic()
+	martini_app := martini.Classic()
 
-    martini_app.Use(
-        render.Renderer(
-            render.Options{
-                Directory: "server/views",
-            },
-        ),
-    )
-    martini_app.Use(
-        martini.Static("client/public"),
-    )
+	martini_app.Use(
+		render.Renderer(
+			render.Options{
+				Directory: "server/views",
+			},
+		),
+	)
+	martini_app.Use(
+		martini.Static("client/public"),
+	)
 
-    app := Main{}
-    app.route(martini_app)
+	app := Main{}
+	app.route(martini_app)
 
-    fmt.Printf("App starting!\n")
+	fmt.Printf("App starting!\n")
 
-    martini_app.Run()
+	martini_app.Run()
 }
